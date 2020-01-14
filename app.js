c4e2741dd6d3b3e5e50fcb59cbf62e6494baf530
juyonglee <juyonglee0208@gmail.com>
2020-01-14 23:42:32 +0900

    API 문서를 보기 위한 Swagger-UI 설정 추가

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// Swagger 사용을 위한 Module 추가
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./public/documentation/swagger.json');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
