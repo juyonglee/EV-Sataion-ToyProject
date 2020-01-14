7d1cea6b4578fa9ec6cfc67d470d26d047b8d09d
juyonglee <juyonglee0208@gmail.com>
2020-01-14 23:31:10 +0900

    Test Set의 정보 및 개수를 출력하는 Router 정의 추가

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/testset', function(req, res, next) {
+  const search = fs.readdirSync('./public/testSet');
+  search.forEach((value, index)=>{
+    const samples = fs.readFileSync(`./public/testSet/${value}`);
+    const sampleJSON = JSON.parse(samples);
+    res.write(`File ${index+1}: ${value}\n`);
+    res.write(`${sampleJSON['Description']}\n`);
+    res.write(`Count: ${sampleJSON['chargerList'].length}\n`);
+    res.write('==============================\n');
+  });
+  res.end();
 });
 
 module.exports = router;
